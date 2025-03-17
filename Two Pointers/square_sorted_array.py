847beb66d99fe7ce1d405960b888e3acaf325248
Arshiyan <Arshiyankhan92@gmail.com>
2025-03-17 12:36:41 +0500

    Updated Two Pointers approach for Squaring Sorted Array

--- Two Pointers/square_sorted_array.py
+++ Two Pointers/square_sorted_array.py
@@ -1,18 +1,20 @@
+def sorted_squares(nums):
+    n = len(nums)
+    left, right = 0, n - 1
+    result = [0] * n
+    index = n - 1
+
+    while left <= right:
+        if abs(nums[left]) > abs(nums[right]):
+            result[index] = nums[left] ** 2
+            left += 1
+        else:
+            result[index] = nums[right] ** 2
+            right -= 1
+        index -= 1
+
+    return result
+
+
 nums = [-4, -1, 0, 3, 10]
-n = len(nums)
-left = 0
-right = n-1
-result = [0] * n
-index = n-1
-
-while left <= right:
-    if abs(nums[left]) > abs(nums[right]):
-        result[index] = nums[left] ** 2
-        left += 1
-    
-    else:
-        result[index] = nums[right] ** 2
-        right -= 1
-    index -= 1
-
-print(result)
+print(sorted_squares(nums))  
